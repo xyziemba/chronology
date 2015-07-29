de3e9ff5dc51fc0c235bc11be5c2dd300942d94a
Xy Ziemba <xy.ziemba@gmail.com>
2015-07-29 15:31:28 -0700

    TravisCI: Modify libgit2 setup such that pygit2 can compile and load.

---
+++ local_libgit2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+if [ -z ${LIBGIT2+x} ]; then
+	echo "Variable LIBGIT2 is not set. Please specify the location for LIBGIT2.";
+	exit 1;
+fi
+
+if [ ! -d "$LIBGIT2/lib" ]; then
+	curl -L -o libgit2-0.22.3.tar.gz https://github.com/libgit2/libgit2/archive/v0.22.3.tar.gz;
+	tar xzf libgit2-0.22.3.tar.gz;
+	cd libgit2-0.22.3;
+	mkdir build;
+	cd build;
+	cmake .. -DBUILD_CLAR=NO -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$LIBGIT2;
+	make install;
+else
+	echo 'Using cached copy of libgit2.';
+fi
